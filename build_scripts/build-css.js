fe14b8e25f93db36958a27acd85cd334f3aaf406
ksneo <vkashapov@gmail.com>
2017-11-29 16:15:33 +0300

    Fix break gulp on sass errors

--- build_scripts/build-css.js
+++ build_scripts/build-css.js
@@ -2,15 +2,20 @@ import gulp from 'gulp';
 import gulpLoaderPlugins from 'gulp-load-plugins';
 import browserSyncTool from 'browser-sync';
 import {root, paths, resource} from './settings';
+import {gulpSassError} from 'gulp-sass-error';
 
 const $ = gulpLoaderPlugins();
+const throwError = false;
 
 // compile Sass -> CSS
 gulp.task('build:sass', () => {
     const browserSync = browserSyncTool.get('sync');
     return gulp.src(resource.src.sass)
       .pipe($.plumber())
-      .pipe($.sass())
+      .pipe(
+        $.sass()
+        .on('error', gulpSassError(throwError))
+      )
       .pipe($.concat('style.css'))
       .pipe($.pleeease())
       .pipe(gulp.dest(paths.dist.css))
